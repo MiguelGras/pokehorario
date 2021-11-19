5abb2870338a77a8e9d42599d70e5ad251115691
Miquel <6692.joan23@fje.edu>
2021-11-19 18:31:53 +0100

    ejercicio 3

--- js/code.js
+++ js/code.js
@@ -19,3 +19,26 @@ function saludo() {
         document.body.style.backgroundColor = rgb(238, 107, 47);
     }
 }
+
+window.onload = function() {
+    saludo();
+    console.log(getListRandom(10, 1, 10));
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function getListRandom(n, min, max) {
+    var array = []
+    for (let i = 0; i < n; i++) {
+        let numr = getRndInteger(min, max);
+        if (array.includes(numr)) {
+            i--;
+        } else {
+            array.push(numr);
+        }
+    }
+    return array;
+}
